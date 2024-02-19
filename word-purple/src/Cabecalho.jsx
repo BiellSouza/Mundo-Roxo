ffaa0848a53f7fc1066f163c936102da54682912
Biell <gabrielsouza65115@gmail.com>
2024-02-19 19:27:13 -0300

    feat: Insere a estilização do cabeçalho para tela para dispositivo maior que mobile e menor que desktop

--- word-purple/src/Cabecalho.jsx
+++ word-purple/src/Cabecalho.jsx
@@ -1,15 +1,15 @@
 function Cabecalho() {
     return (
-        <header className="cabecalho flex justify-between items-center py-[5px] shadow-md w-[100%] fixed bg-white">
+        <header className="cabecalho flex justify-between items-center py-[5px] shadow-md w-[100%] fixed bg-white md:h-[8vh]">
             <div className="logo flex items-center">
                 <img src="" alt="" />
 
-                <h1 class='hidden'><span>P</span>urple<span>W</span>orld</h1>
+                <h1 class='hidden md:block relative left-[10px] text-[25px] md:font-bold'><span class='text-purple-700'>P</span>urple<span class='md:text-purple-700'>W</span>orld</h1>
 
             </div>
 
             <nav className="navigation flex gap-[90px]">
-                <ul className="nav-list flex gap-[10px] cursor-pointer">
+                <ul className="nav-list flex gap-[10px] md:gap-[30px] md: pl-[30px] text-[25px] cursor-pointer">
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Home</a></li>
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Menu</a></li>
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Promotions</a></li>
@@ -18,8 +18,8 @@ function Cabecalho() {
             </nav>
 
             <div className="icones flex mr-[20px] pt-[6px] pl-[5px] text-purple-800">
-                <a href="#" className="car ml-[5px] relative left-[2px] hover:text-red-800 transition-[0.2s] cursor-pointer"><ion-icon name="search-outline"></ion-icon></a>
-                <a href="#" className="search ml-[5px] relative left-[10px] hover:text-red-800 transition-[0.2s] cursor-pointer"><ion-icon name="lock-closed-outline"></ion-icon></a>
+                <a href="#" className="car ml-[5px] relative left-[2px] md:mr-[20px] md:text-[25px] hover:text-red-800 transition-[0.2s] cursor-pointer"><ion-icon name="search-outline"></ion-icon></a>
+                <a href="#" className="search ml-[5px] relative left-[10px] md:text-[25px] hover:text-red-800 transition-[0.2s] cursor-pointer"><ion-icon name="lock-closed-outline"></ion-icon></a>
             </div>
         </header>
     )
