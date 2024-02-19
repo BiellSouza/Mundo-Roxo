1eac4c1e38cbd0b899c359f7fb5bfb97eb105393
Biell <gabrielsouza65115@gmail.com>
2024-02-19 07:52:25 -0300

    fix: Corrige a estilização do cabeçalho para página mobile

--- word-purple/src/Cabecalho.jsx
+++ word-purple/src/Cabecalho.jsx
@@ -1,25 +1,25 @@
 function Cabecalho() {
     return (
-        <header>
-            <div className="logo">
+        <header className="cabecalho flex justify-between items-center py-[5px] shadow-md w-[100%] fixed bg-white">
+            <div className="logo flex items-center">
                 <img src="" alt="" />
 
-                <h1><span>P</span>urple<span>W</span>orld</h1>
+                <h1 class='hidden'><span>P</span>urple<span>W</span>orld</h1>
 
             </div>
 
-            <nav className="navigation">
-                <ul className="nav-list">
-                    <li className="nav-item"><a href="#">Home</a></li>
-                    <li className="nav-item"><a href="#">Menu</a></li>
-                    <li className="nav-item"><a href="#">Promotions</a></li>
-                    <li className="nav-item"><a href="#">Suport</a></li>
+            <nav className="navigation flex gap-[90px]">
+                <ul className="nav-list flex gap-[10px] cursor-pointer">
+                    <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Home</a></li>
+                    <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Menu</a></li>
+                    <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Promotions</a></li>
+                    <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Suport</a></li>
                 </ul>
             </nav>
 
-            <div className="icones">
-                <a href="#" className="car"><ion-icon name="search-outline"></ion-icon></a>
-                <a href="#" className="search"><ion-icon name="lock-closed-outline"></ion-icon></a>
+            <div className="icones flex mr-[20px] pt-[6px] pl-[5px] text-purple-800">
+                <a href="#" className="car ml-[5px] relative left-[2px] hover:text-red-800 transition-[0.2s] cursor-pointer"><ion-icon name="search-outline"></ion-icon></a>
+                <a href="#" className="search ml-[5px] relative left-[10px] hover:text-red-800 transition-[0.2s] cursor-pointer"><ion-icon name="lock-closed-outline"></ion-icon></a>
             </div>
         </header>
     )
