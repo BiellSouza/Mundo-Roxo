5fedd82f9afb38cb352a326357c355da6f7f0381
Biell <gabrielsouza65115@gmail.com>
2024-02-24 15:54:54 -0300

    fix: Corrige as posições do Cabeçalho e Meio para telas até 768

--- word-purple/src/Cabecalho.jsx
+++ word-purple/src/Cabecalho.jsx
@@ -1,15 +1,15 @@
 function Cabecalho() {
     return (
-        <header className="cabecalho shadow-sm w-[100%] h-[8vh] flex items-center justify-between sm:h-[9vh]">
-            <div className="logo">
-                <img class='hidden sm:hidden' src="./src/assets/png-logo.png" alt="" />
+        <header className="cabecalho shadow-sm w-[100%] h-[8vh] flex items-center justify-between sm:h-[9vh] ">
+            <div className="logo md:flex md:flex-row md:items-center md:w-[10%]">
+                <img class='hidden sm:hidden md:block md:w-[50%] md:ml-[10%] ' src="./src/assets/png-logo.png" alt="" />
 
-                <h1 class='font-bold hidden sm:block sm:pl-[30%] text-xl text-red-700'><span class='text-purple-700'>P</span>urple<span class='text-purple-700'>W</span>orld</h1>
+                <h1 class='font-bold hidden sm:block sm:pl-[30%] text-xl text-red-700 md:pl-[5%]'><span class='text-purple-700'>P</span>urple<span class='text-purple-700'>W</span>orld</h1>
 
             </div>  
 
             <nav className="navigation">
-                <ul className="nav-list font-bold flex flex-row relative right-[20%] gap-[8%] sm:text-[110%] sm:relative sm:left-[2%] hover:text-red-700 transition-[0.5s] ">
+                <ul className="nav-list font-bold flex flex-row relative right-[20%] gap-[8%] sm:text-[110%] sm:relative sm:left-[2%] md:relative hover:text-red-700 transition-[0.5s] ">
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Home</a></li>
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Menu</a></li>
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Promotions</a></li>
@@ -17,7 +17,7 @@ function Cabecalho() {
                 </ul>
             </nav>
 
-            <div className="icones flex relative right-[6%] top-[5%] text-md gap-[30%] sm:text-[110%] text-purple-700">
+            <div className="icones flex relative right-[6%] top-[5%] text-md gap-[30%] sm:text-[110%] md:gap-[50%] text-purple-700">
                 <a href="#" className="car  hover:text-red-700 transition-[0.5s] "><ion-icon name="search-outline"></ion-icon></a>
                 <a href="#" className="search  hover:text-red-700 transition-[0.5s]"><ion-icon name="lock-closed-outline"></ion-icon></a>
             </div>
--- word-purple/src/Meio.jsx
+++ word-purple/src/Meio.jsx
@@ -8,9 +8,9 @@ function Meio() {
                     <h2 class='font-bold text-purple-700 text-xl sm:pt-[3%] sm:text-[190%]'>Uma vida</h2>
                 </div>
 
-                <div className="hero-img w-[100%] h-[100vh] sm:h-[110vh]">
+                <div className="hero-img w-[100%] h-[100vh] sm:h-[110vh] md:h-[130vh]">
                     <img class='flex w-[100%]' src="./src/assets/png-meio.png" alt="" />
-                    <button class='flex relative left-[37%] top-[5%] border-2 px-[3%] py-[3%] sm:relative sm:left-[40%] border-red-700 rounded-[10%] hover:scale-[1.2]' type="submit" className="btn-pedido" id="chamaform">Peça o seu</button>
+                    <button class='flex relative left-[37%] top-[5%] border-2 px-[3%] py-[3%] sm:relative sm:left-[40%] md:left-[43%] border-red-700 rounded-[10%] hover:scale-[1.2]' type="submit" className="btn-pedido" id="chamaform">Peça o seu</button>
                 </div>
             </section>
         </main>
