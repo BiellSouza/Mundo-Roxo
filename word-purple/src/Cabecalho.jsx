40ce9724a1c1667704b1c186756a52818ec445de
Biell <gabrielsouza65115@gmail.com>
2024-02-24 15:26:29 -0300

    fix: Corrige as posições do cabeçalho e Meio para telas até 768

--- word-purple/src/Cabecalho.jsx
+++ word-purple/src/Cabecalho.jsx
@@ -1,15 +1,15 @@
 function Cabecalho() {
     return (
-        <header className="cabecalho shadow-sm w-[100%] h-[8vh] flex items-center justify-between">
+        <header className="cabecalho shadow-sm w-[100%] h-[8vh] flex items-center justify-between sm:h-[9vh]">
             <div className="logo">
                 <img class='hidden sm:hidden' src="./src/assets/png-logo.png" alt="" />
 
-                <h1 class='font-bold hidden text-red-700'><span class='text-purple-700'>P</span>urple<span class='text-purple-700'>W</span>orld</h1>
+                <h1 class='font-bold hidden sm:block sm:pl-[30%] text-xl text-red-700'><span class='text-purple-700'>P</span>urple<span class='text-purple-700'>W</span>orld</h1>
 
             </div>  
 
             <nav className="navigation">
-                <ul className="nav-list font-bold flex flex-row relative right-[20%] gap-[8%] hover:text-red-700 transition-[0.5s] ">
+                <ul className="nav-list font-bold flex flex-row relative right-[20%] gap-[8%] sm:text-[110%] sm:relative sm:left-[2%] hover:text-red-700 transition-[0.5s] ">
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Home</a></li>
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Menu</a></li>
                     <li className="nav-item text-purple-800 hover:text-red-500 transition-[0.2s] cursor-pointer"><a href="#">Promotions</a></li>
@@ -17,9 +17,9 @@ function Cabecalho() {
                 </ul>
             </nav>
 
-            <div className="icones flex relative right-[6%] top-[5%] text-md gap-[30%] text-purple-700 hover:text-red-700 transition-[0.5s]">
-                <a href="#" className="car "><ion-icon name="search-outline"></ion-icon></a>
-                <a href="#" className="search"><ion-icon name="lock-closed-outline"></ion-icon></a>
+            <div className="icones flex relative right-[6%] top-[5%] text-md gap-[30%] sm:text-[110%] text-purple-700">
+                <a href="#" className="car  hover:text-red-700 transition-[0.5s] "><ion-icon name="search-outline"></ion-icon></a>
+                <a href="#" className="search  hover:text-red-700 transition-[0.5s]"><ion-icon name="lock-closed-outline"></ion-icon></a>
             </div>
         </header>
     )
--- word-purple/src/Meio.jsx
+++ word-purple/src/Meio.jsx
@@ -3,14 +3,14 @@ function Meio() {
         <main class='flex pt-[10%] w-[100%] h-[auto]'>
             <section className="hero">
                 <div className="hero-text flex flex-col text-center">
-                    <h4 class='font-bold text-purple-700 text-lg'>Welcome to the Purple World</h4>
-                    <h1 class=' font-bold text-red-700 text-lg'>Mais que um sabor</h1>
-                    <h2 class='font-bold text-purple-700 text-xl'>Uma vida</h2>
+                    <h4 class='font-bold text-purple-700 text-lg sm:text-[200%]'>Welcome to the Purple World</h4>
+                    <h1 class=' font-bold text-red-700 text-lg sm:text-[190%] sm:pt-[3%]'>Mais que um sabor</h1>
+                    <h2 class='font-bold text-purple-700 text-xl sm:pt-[3%] sm:text-[190%]'>Uma vida</h2>
                 </div>
 
-                <div className="hero-img w-[100%] h-[100vh]">
+                <div className="hero-img w-[100%] h-[100vh] sm:h-[110vh]">
                     <img class='flex w-[100%]' src="./src/assets/png-meio.png" alt="" />
-                    <button class='flex  relative left-[37%] top-[5%] border-2 px-[3%] py-[3%] border-red-700 rounded-[10%] hover:scale-[1.2]' type="submit" className="btn-pedido" id="chamaform">Peça o seu</button>
+                    <button class='flex relative left-[37%] top-[5%] border-2 px-[3%] py-[3%] sm:relative sm:left-[40%] border-red-700 rounded-[10%] hover:scale-[1.2]' type="submit" className="btn-pedido" id="chamaform">Peça o seu</button>
                 </div>
             </section>
         </main>
